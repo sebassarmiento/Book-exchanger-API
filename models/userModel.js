3689f4eb9c85c309a47654b089df761748fbad81
Sebastian Sarmiento <sebi_sarmiento@hotmail.com>
2018-12-21 21:32:05 -0500

    Updated user model

--- models/userModel.js
+++ models/userModel.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose');
 
 const User = mongoose.Schema({
     email: { type: String, required: true },
-    password: { type: String, required: true }
+    password: { type: String, required: true },
+    username: { type: String, required: true },
+    gender: { type: String, required: true },
+    age: { type: Number, required: true }
 })
 
 module.exports = mongoose.model('User', User);
