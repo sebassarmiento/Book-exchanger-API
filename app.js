ef4894ea51fa4a17d1235c2a6855f56f2e055482
Sebastian Sarmiento <sebi_sarmiento@hotmail.com>
2018-12-21 21:38:35 -0500

    Added login route

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 
 const signUpRouter = require('./routes/signUp');
+const loginRouter = require('./routes/login');
 
 mongoose.connect('mongodb+srv://sebastian:sarmiento@cluster0-ok6xa.mongodb.net/book-exchange?retryWrites=true', {useNewUrlParser: true})
 
@@ -11,6 +12,7 @@ app.use(express.json())
 app.use(cors())
 
 app.use('/signup', signUpRouter)
+app.use('/login', loginRouter)
 
 app.use((req, res, next) => {
     const error = new Error('Not found')
---
+++ routes/login.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+
+const User = require('../models/userModel');
+
+router.post('/', (req, res) => {
+    User.find({email: req.body.email})
+    .then(result => {
+        if(result && result.length !== 0){
+            if(result[0].password === req.body.password){
+                res.status(200).json({message: "Access granted"})
+            } else {
+                res.status(500).json({message: "Invalid data"})
+            }
+        } else {
+            res.status(500).json({message: "Invalid data"})
+        }
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({error: err})
+    })
+})
+
+module.exports = router
