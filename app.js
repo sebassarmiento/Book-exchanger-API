0e3e8bc3daefd40e6553f761379f3cb48b78b5d6
Sebastian Sarmiento <sebi_sarmiento@hotmail.com>
2018-12-27 12:16:47 -0500

    Adding token validation

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express()
 const cors = require('cors');
 const mongoose = require('mongoose');
 
+const validToken = require('./middleware/validToken');
+
 const signUpRouter = require('./routes/signUp');
 const loginRouter = require('./routes/login');
 const booksRouter = require('./routes/books');
@@ -14,7 +16,7 @@ app.use(cors())
 
 app.use('/signup', signUpRouter)
 app.use('/login', loginRouter)
-app.use('/books', booksRouter)
+app.use('/books', validToken, booksRouter)
 
 app.use((req, res, next) => {
     const error = new Error('Not found')
